97d429c41094c498baa2abba949d995894d5f7ec
DoggiKim <kmss0426@hanyang.ac.kr>
2025-05-08 16:06:13 +0900

    Upload Homework6 Folder

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// import logo from './logo.svg';
+import React from 'react';
+import {Header} from './Header';
+import {Nav} from './Nav';
+import {Create} from './Create';
+
+
+// import './App.css';
+
+function App() {
+  const [index, setIndex] = React.useState(['제목1', '제목2', '제목3']);
+  const [input, setInput] = React.useState('');
+  
+  const IndexAdd = () => {
+    setIndex([...index,input]);
+    setInput('');
+  }
+  
+
+return (
+    <div>
+      <Header title="ERICA x React Props State" />
+      <Nav index = {index}/>
+      <Create 
+        inputIndex = {input}
+        setInput={setInput}
+        IndexAdd={IndexAdd}/>
+    </div>
+  )
+
+}
+
+export default App;
